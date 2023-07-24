f708aaf72201fc636d49d31828d505e3a23af1b4
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2023-07-25 02:28:35 +0300

    first version of giannis' pluto cards . No thumbnail yet-no video preview of notebook

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        'xxs': '0.5rem'
+      },
+      colors: {
+        'julia-blue': {
+          light: '#4e64ae',
+          dark: '#3e5294'
+        },
+        'julia-green': {
+          light: '#399746',
+          dark: '#2a7b35',
+        },
+
+        'julia-red': {
+          light: '#c93c32',
+          dark: '#8a2d2d'
+        },
+        'julia-purple': {
+          light: '#9259a3',
+          dark: '#6b4178',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
