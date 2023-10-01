a8829d5eeaee4a8ec2bc60b12737774631d93355
GiannisTsagkaropoulos <johntsagas2003@gmail.com>
2023-10-01 16:37:53 +0300

    fix responsiveness of 404 page

--- pages/404.js
+++ pages/404.js
@@ -5,34 +5,33 @@ import classes from "../styles/404page.module.css";
 function Custom404() {
   return (
     <section className="w-full">
-      <div className="h-[5rem]"></div>
-      <h1 className="text-3xl lg:text-6xl tracking-wide text-gray-700 font-extrabold mx-auto text-center w-[60%]">
-        404
-      </h1>
-      <div className="text-center">
+      <div className="h-screen">
+        <div className="h-[5rem]"></div>
+        <h1 className="text-4xl lg:text-6xl tracking-wide text-gray-700 font-extrabold mx-auto text-center">
+          404
+        </h1>
         <div
-          className="h-[25rem] bg-center bg-no-repeat"
+          className="h-[15rem] md:h-[25rem] bg-center bg-no-repeat"
           style={{
             backgroundImage:
               "url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif)",
             backgroundSize: "cover", // Set background-size to cover
           }}
-        ></div>
-        <div>
-          <h3 className="text-3xl 2xl:text-[80px] tracking-wide text-gray-700 font-extrabold mx-auto text-center my-4 w-[60%]">
-            Look like you're lost
-          </h3>
-          <p className="text-lg lg:text-xl text-julia-blue-light opacity-70 tracking-wider font-medium w-[75%] mx-auto  text-justify lg:text-center">
-            The page you are looking for not avaible!
-          </p>
-          <div className="h-4"></div>
-          <Link href="/">
-            <div className="w-max rounded-lg bg-julia-blue-light hover:bg-julia-blue-dark px-8 py-3 text-center text-base font-medium text-white mx-auto mb-8">
-              Go to Home
-            </div>
-          </Link>
-        </div>
+        />
+        <h3 className="text-3xl 2xl:text-[80px] tracking-wide text-gray-700 font-extrabold mx-auto text-center">
+          Look like you're lost
+        </h3>
+        <p className="text-lg lg:text-xl text-julia-blue-light opacity-70 tracking-wider font-medium mx-auto text-center">
+          The page you are looking for not avaible!
+        </p>
+        <div className="h-4"></div>
+        <Link href="/">
+          <div className="w-max rounded-lg bg-julia-blue-light hover:bg-julia-blue-dark px-8 py-3 text-center text-base font-medium text-white mx-auto mb-8">
+            Go to Home
+          </div>
+        </Link>
       </div>
+      <div className="h-8"></div>
     </section>
   );
 }
