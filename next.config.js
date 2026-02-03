9af4a9831cca07e94b4a36c9d7093453f9c42c8b
Giannis Tsagkaropoulos <giannis.tsagkaropoulos@gmail.com>
2026-02-03 14:33:03 +0100

    chore: bump dependency versions to fix severities & add turbopack to nextJS's config

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   // Optionally, add any other Next.js config below
   reactStrictMode: true,
+  turbopack: {}
 };
 
 // Merge MDX config with Next.js config
